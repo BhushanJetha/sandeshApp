10e4e6a4e96579f277628ba3030fa4457fdab61b
abhijit0674 <abhishewale0674@gmail.com>
2020-01-19 15:58:06 +0530

    logout option implement
    FAQFragment UI design ready

--- app/src/main/java/com/aystech/sandesh/activity/MainActivity.java
+++ app/src/main/java/com/aystech/sandesh/activity/MainActivity.java
@@ -1,26 +1,32 @@
 package com.aystech.sandesh.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.aystech.sandesh.R;
 import com.aystech.sandesh.fragment.DashboardFragment;
+import com.aystech.sandesh.fragment.LoginFragment;
 import com.aystech.sandesh.fragment.MobileVerificationFragment;
 import com.aystech.sandesh.utils.Constants;
 import com.aystech.sandesh.utils.FragmentUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView imgLogout;
     Toolbar toolBar;
     ConstraintLayout clDashboard, clOther;
     ConstraintLayout bottomBar;
 
     MobileVerificationFragment mobileVerificationFragment;
     DashboardFragment dashboardFragment;
+    LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         mobileVerificationFragment = (MobileVerificationFragment) Fragment.instantiate(this, MobileVerificationFragment.class.getName());
         dashboardFragment = (DashboardFragment) Fragment.instantiate(this, DashboardFragment.class.getName());
+        loginFragment = (LoginFragment) Fragment.instantiate(this, LoginFragment.class.getName());
 
         if (Constants.fragmentType.equals("Dashboard")) {
             FragmentUtil.commonMethodForFragment(getSupportFragmentManager(), dashboardFragment, R.id.frame_container,
@@ -40,15 +47,21 @@ public class MainActivity extends AppCompatActivity {
                     false);
         }
 
+        onClickListener();
     }
 
     private void initView() {
         toolBar = findViewById(R.id.toolBar);
         setSupportActionBar(toolBar);
+        imgLogout = findViewById(R.id.imgLogout);
 
         clDashboard = findViewById(R.id.clDashboard);
         clOther = findViewById(R.id.clOther);
         bottomBar = findViewById(R.id.bottomBar);
+    }
+
+    private void onClickListener() {
+        imgLogout.setOnClickListener(this);
     }
 
     public void setUpToolbar(boolean isToolBar, boolean isDashboard, String toolbarTitle,
@@ -76,5 +89,37 @@ public class MainActivity extends AppCompatActivity {
             toolBar.setVisibility(View.GONE);
             bottomBar.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imgLogout:
+                openLogoutDialog();
+                break;
+        }
+    }
+
+    private void openLogoutDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Do you want logout?")
+                .setMessage("Are you sure, do you want to logout?")
+                .setCancelable(true)
+                .setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+
+                        FragmentUtil.commonMethodForFragment(getSupportFragmentManager(), loginFragment, R.id.frame_container,
+                                false);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
---
+++ app/src/main/java/com/aystech/sandesh/fragment/FAQFragment.java
@@ -0,0 +1,107 @@
+package com.aystech.sandesh.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import com.aystech.sandesh.R;
+import com.aystech.sandesh.adapter.CustomExpandableListAdapter;
+import com.aystech.sandesh.utils.ExpandableListDataFAQ;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FAQFragment extends Fragment {
+
+    Context context;
+
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
+    public FAQFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_faq, container, false);
+
+        initView(view);
+
+        bindDataToUI();
+
+        onClickListener();
+
+        return view;
+    }
+
+    private void initView(View view) {
+        expandableListView = view.findViewById(R.id.expandableListView);
+    }
+
+    private void bindDataToUI() {
+        expandableListDetail = ExpandableListDataFAQ.getData();
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+        expandableListAdapter = new CustomExpandableListAdapter(context, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+    }
+
+    private void onClickListener() {
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(context,
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(context,
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        context,
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+    }
+}
