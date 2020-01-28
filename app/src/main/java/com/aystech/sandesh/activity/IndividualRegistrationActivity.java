99ca0264edbb8f90666e8d65c0c6cf089d9c499a
abhijit0674 <abhishewale0674@gmail.com>
2020-01-28 19:02:58 +0530

    image selection from gallery.

--- app/src/main/java/com/aystech/sandesh/activity/IndividualRegistrationActivity.java
+++ app/src/main/java/com/aystech/sandesh/activity/IndividualRegistrationActivity.java
@@ -1,20 +1,20 @@
 package com.aystech.sandesh.activity;
 
-import android.Manifest;
+import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -35,7 +35,13 @@ import com.aystech.sandesh.utils.Uitility;
 import com.aystech.sandesh.utils.ViewProgressDialog;
 import com.google.gson.JsonObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -54,6 +60,9 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
     private ViewProgressDialog viewProgressDialog;
     private LinearLayout llDateOfBirth, llProfilePiture;
     private int mYear, mMonth, mDay;
+    Uri picUri;
+    Bitmap myBitmap;
+    private String strProfileBase64;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +70,7 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_individual_registration);
 
         init();
+
         onClick();
     }
 
@@ -115,7 +125,7 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
                                     if (!strPassword.isEmpty()) {
                                         if (!strReEnteredPassword.isEmpty()) {
                                             if (strPassword.equals(strReEnteredPassword)) {
-                                                doRigistrationAPICall();
+                                                doRegistrationAPICall();
                                             } else {
                                                 Uitility.showToast(IndividualRegistrationActivity.this, "Password and re-Entered Password not matched !!");
                                             }
@@ -174,45 +184,12 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
         llProfilePiture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // custom dialog
-                final Dialog dialog = new Dialog(IndividualRegistrationActivity.this);
-                dialog.setContentView(R.layout.image_selection_dialog);
-
-                LinearLayout llCamera, llGallery;
-                Button btnCancel;
-
-                llCamera = dialog.findViewById(R.id.llCamera);
-                llGallery = dialog.findViewById(R.id.llGallery);
-                btnCancel = dialog.findViewById(R.id.btnCancel);
-
-
-                llCamera.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                    }
-                });
-
-                llGallery.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                    }
-                });
-
-                btnCancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-                });
-
-
-                dialog.show();
+                gotoSelectPicture();
             }
         });
     }
 
-    private void doRigistrationAPICall() {
+    private void doRegistrationAPICall() {
         ViewProgressDialog.getInstance().showProgress(this);
 
         JsonObject jsonObject = new JsonObject();
@@ -225,7 +202,7 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
         jsonObject.addProperty("gender", strGender);
         jsonObject.addProperty("birth_date", strBirthDate);
         jsonObject.addProperty("refferal_code", strRefferalCode);
-        jsonObject.addProperty("profile_img", "");
+        jsonObject.addProperty("profile_img", strProfileBase64);
         jsonObject.addProperty("fcm_id", strFCMId);
 
         ApiInterface apiInterface = RetrofitInstance.getClient();
@@ -281,5 +258,138 @@ public class IndividualRegistrationActivity extends AppCompatActivity {
                     }
                 }, mYear, mMonth, mDay);
         datePickerDialog.show();
+    }
+
+    private void gotoSelectPicture() {
+        startActivityForResult(getPickImageChooserIntent(), 200);
+    }
+
+    private Intent getPickImageChooserIntent() {
+        // Determine Uri of camera image to save.
+        Uri outputFileUri = getCaptureImageOutputUri();
+
+        List<Intent> allIntents = new ArrayList<>();
+
+        // collect all camera intents
+        Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        List<ResolveInfo> listCam = getPackageManager().queryIntentActivities(captureIntent, 0);
+        for (ResolveInfo res : listCam) {
+            Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(res.activityInfo.packageName);
+            if (outputFileUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            }
+            allIntents.add(intent);
+        }
+
+        // collect all gallery intents
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        List<ResolveInfo> listGallery = getPackageManager().queryIntentActivities(galleryIntent, 0);
+        for (ResolveInfo res : listGallery) {
+            Intent intent = new Intent(galleryIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(res.activityInfo.packageName);
+            allIntents.add(intent);
+        }
+
+        Intent mainIntent = allIntents.get(allIntents.size() - 1);
+        for (Intent intent : allIntents) {
+            if (Objects.requireNonNull(intent.getComponent()).getClassName().equals("com.android.documentsui.DocumentsActivity")) {
+                mainIntent = intent;
+                break;
+            }
+        }
+        allIntents.remove(mainIntent);
+
+        // Create a chooser from the main intent
+        Intent chooserIntent = Intent.createChooser(mainIntent, "Select source");
+
+        // Add all other intents
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, allIntents.toArray(new Parcelable[allIntents.size()]));
+
+        return chooserIntent;
+    }
+
+    /**
+     * Get URI to image received from capture by camera.
+     */
+    private Uri getCaptureImageOutputUri() {
+        Uri outputFileUri = null;
+        File getImage = getExternalCacheDir();
+        if (getImage != null) {
+            outputFileUri = Uri.fromFile(new File(getImage.getPath(), "profile.png"));
+        }
+        return outputFileUri;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap;
+        if (resultCode == Activity.RESULT_OK) {
+            if (getPickImageResultUri(data) != null) {
+                picUri = getPickImageResultUri(data);
+
+                try {
+                    myBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picUri);
+                    myBitmap = getResizedBitmap(myBitmap, 500);
+
+                    strProfileBase64 = getStringImage(myBitmap);
+
+                    ivProfilePicture.setImageBitmap(myBitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                bitmap = (Bitmap) data.getExtras().get("data");
+                myBitmap = bitmap;
+
+                strProfileBase64 = getStringImage(myBitmap);
+
+                ivProfilePicture.setImageBitmap(myBitmap);
+            }
+        }
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 0) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    /**
+     * Get the URI of the selected image from {@link #getPickImageChooserIntent()}.<br />
+     * Will return the correct URI for camera and gallery image.
+     *
+     * @param data the returned data of the activity result
+     */
+    public Uri getPickImageResultUri(Intent data) {
+        boolean isCamera = true;
+        if (data != null) {
+            String action = data.getAction();
+            isCamera = action != null && action.equals(MediaStore.ACTION_IMAGE_CAPTURE);
+        }
+
+        return isCamera ? getCaptureImageOutputUri() : data.getData();
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+        byte[] imageBytes = outputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 }
